2cf736702c0f0eecae3510d2aca35b4e41b08ec1
Swapnil-kus-1503 <ashukushwaha15031997@gmail.com>
2022-05-17 21:41:00 +0530

    counter

---
+++ src/components/Counter.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Counter = () => {
+
+    const [count, setCount] = React.useState(0);
+
+
+    // const incCount = () => {
+    //     setCount(count+1);
+    //     console.log(count);
+    // }
+
+    // const decCount = () => {
+    //     setCount(count-1);
+    //     console.log(count);
+    // }
+
+    return (
+        <div>
+            <h1>Likes Counter:{count}</h1>
+            {/* <button onClick={incCount}>Increment</button>
+            
+            <button onClick={decCount}>Decrement</button> */}
+
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
+        </div>
+    );
+};
+
+export default Counter;
